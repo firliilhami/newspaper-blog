d04883f5d54ff33607710b1c0beb8f009dccce02
Firli Ilhami <firli.ilhami@gmail.com>
2023-03-24 14:07:38 +0700

    newspaper project

--- articles/views.py
+++ articles/views.py
@@ -1,38 +1,45 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Article
 
 
 # Create your views here.
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
     fields = (
         "title",
-        "body",
-        "author"
+        "body"
     )
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleUpdateView(UpdateView):
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = (
         "title",
@@ -45,3 +52,7 @@ class ArticleUpdateView(UpdateView):
         print('---')
         print(self.kwargs.items())
         return reverse_lazy('article_detail', kwargs= {'pk':self.kwargs['pk']})
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
